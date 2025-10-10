dce450835134005b9fec784a45873215816c0a68
hoanghai81 <102153932+hoanghai81@users.noreply.github.com>
2025-10-10 22:09:07 +0700

    https://vnepg.site/epgu.xml

--- scripts/extract_tvg_ids.py
+++ scripts/extract_tvg_ids.py
@@ -2,7 +2,7 @@ import requests
 import gzip
 import xml.etree.ElementTree as ET
 
-URL = "https://lichphatsong.site/schedule/epg.xml.gz"
+URL = "https://vnepg.site/epgu.xml"
 OUTPUT = "docs/tvg_ids.txt"
 
 print("=== BẮT ĐẦU TẢI EPG ===")
